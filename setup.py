d5bd93aaa1040c9536d70c312a7660d2b6334f39
Thijsvanede <thijsvanede@hotmail.com>
2017-06-18 15:02:16 +0200

    Added code + setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'bropy',
+	packages = ['bropy'],
+	version = '0.1',
+	description = 'A lightweight bro log reader',
+	author = 'Thijs van Ede',
+	author_email = 't.s.student.utwente.nl',
+	url = 'https://github.com/Thijsvanede/bropy',
+	download_url = 'TODO'
+	keywords = ['bro', 'log', 'reader', 'python']
+	classifiers = [],
+)
